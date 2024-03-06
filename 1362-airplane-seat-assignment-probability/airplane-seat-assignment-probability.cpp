ec2ea855d66d5ac357087d99ec0c145e0d612b7f
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-03-06 10:24:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (17.61%) - LeetSync

--- 1362-airplane-seat-assignment-probability/airplane-seat-assignment-probability.cpp
+++ 1362-airplane-seat-assignment-probability/airplane-seat-assignment-probability.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double nthPersonGetsNthSeat(int n) {
         ios_base::sync_with_stdio(0);
-        if(n==1) return 1;
-        else return 0.5;
+        if(n<=1) return 1;
+        return 0.5;
     }
 };
