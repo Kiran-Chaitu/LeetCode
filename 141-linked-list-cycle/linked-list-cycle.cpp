bb108c897980e1331c4da17505f669594ff4ecad
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-03-06 09:40:02 +0530

    Time: 5 ms (90.85%) | Memory: 10.5 MB (74.71%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,6 +9,7 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        ios_base::sync_with_stdio(0);
         ListNode *s=head,*f=head;
         if(f==NULL) return 0;
         while(f and f->next){
