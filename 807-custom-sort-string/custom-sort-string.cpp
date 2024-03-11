c030a604520caace85aac45a6489ae4490c1aef9
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-03-11 07:04:23 +0530

    Time: 4 ms (26.39%) | Memory: 8.5 MB (23.18%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        unordered_map<char,int> mp1,mp2;
+        for(int i=0;i<order.size();i++){
+            mp1[order[i]]++;
+        }
+        for(int i=0;i<s.size();i++){
+            mp2[s[i]]++;
+        }
+        sort(s.begin(),s.end());
+        string ns="";
+        for(int i=0;i<order.size();i++){
+            while(mp2[order[i]] > 0){
+                ns+=order[i];
+                mp2[order[i]]--;
+            }
+        }
+        for(auto i : mp2){
+            while(i.second > 0){
+                ns+=i.first;
+                i.second--;
+            } 
+        }
+        return ns;
+
+    }
+};
