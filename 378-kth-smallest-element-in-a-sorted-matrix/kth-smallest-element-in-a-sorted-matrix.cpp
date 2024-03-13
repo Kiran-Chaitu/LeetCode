07f1bce648dbe6781738078293d4a1e347c2aecb
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-03-13 11:44:08 +0530

    Time: 29 ms (42.83%) | Memory: 18 MB (19.71%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+        ios_base::sync_with_stdio(0);
         priority_queue<int , vector<int> , greater<int>> pq;
         int n=matrix.size(),i=1;;
         for(int i=0;i<n;i++){
