385fe33a5ff56cf87e3bf2345e0f22810208e10d
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-03-21 22:22:57 +0530

    Time: 40 ms (99.33%) | Memory: 70.2 MB (67.93%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
         int cs=-100000,ts=-100000;
         for(int i=0;i<nums.size();i++){
             cs = max(cs+nums[i] , nums[i]);
