73235fd5bb5ae94a175357ff39547db59631ae2f
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-03-21 11:24:37 +0530

    Time: 152 ms (47.81%) | Memory: 78.9 MB (13.61%) - LeetSync

---
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(!root) return {};
+        if(root->left==NULL and root->right==NULL) return {{root->val}};
+        vector<vector<int>> ans;
+        queue<TreeNode *>q;
+        q.push(root);
+        int k=0;
+        while(!q.empty()){
+            int n=q.size();
+            vector<int> v;
+            for(int i=0;i<n;i++){
+                TreeNode *nn=q.front();
+                if(nn->left!=NULL)q.push(nn->left);
+                if(nn->right!=NULL)q.push(nn->right);
+                v.push_back(nn->val);
+                q.pop();
+            }
+            if(k%2!=0) reverse(v.begin(),v.end());
+            k++;
+            ans.push_back(v);
+        }
+        return ans;
+    }
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        vector<vector<int> > v= levelOrder(root);
+        queue<TreeNode *> q;
+        q.push(root);
+        int k=0;
+        while(!q.empty()){
+            int n=q.size();
+            if(k%2!=0){
+                for(int i=0;i<n;i++){
+                    TreeNode *nn = q.front();
+                    if(nn->left!=NULL) q.push(nn->left);
+                    if(nn->right!=NULL) q.push(nn->right);
+                    nn->val = v[k][i];
+                    q.pop();
+                }
+            }
+            else{
+                for(int i=0;i<n;i++){
+                    TreeNode *nn = q.front();
+                    if(nn->left!=NULL) q.push(nn->left);
+                    if(nn->right!=NULL) q.push(nn->right);
+                    nn->val = v[k][i];
+                    q.pop();
+                }
+            }
+            k++;
+        }
+        return root;
+    }
+};
