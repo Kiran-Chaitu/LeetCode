35a61eed0bde625dd84ab9e5d47f9265841d5de1
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-03-21 21:38:07 +0530

    Time: 4 ms (92.61%) | Memory: 20.6 MB (52.50%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.cpp
@@ -11,37 +11,22 @@
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<int> largestValues(TreeNode* root) {
+        ios_base::sync_with_stdio(0);
         if(!root) return {};
-        if(root->left==NULL and root->right==NULL) return {{root->val}};
-        vector<vector<int>> ans;
+        vector<int> ans;
         queue<TreeNode *>q;
         q.push(root);
         while(!q.empty()){
             int n=q.size();
-            vector<int> v;
+            int maxi = INT_MIN;
             for(int i=0;i<n;i++){
                 TreeNode *nn=q.front();
                 if(nn->left!=NULL)q.push(nn->left);
                 if(nn->right!=NULL)q.push(nn->right);
-                v.push_back(nn->val);
+                if(maxi < nn->val) maxi = nn->val;
                 q.pop();
             }
-            ans.push_back(v);
-        }
-        return ans;
-    }
-    vector<int> largestValues(TreeNode* root) {
-        ios_base::sync_with_stdio(0);
-        vector<vector<int>> v = levelOrder(root);
-        vector<int> ans;
-        for(int i=0;i<v.size();i++){
-            int maxi=INT_MIN;
-            for(int j=0;j<v[i].size();j++){
-                if(v[i][j] > maxi ) maxi = v[i][j];
-                //cout<<v[i][j]<<" ";
-            }
-            //cout<<endl;
             ans.push_back(maxi);
         }
         return ans;
