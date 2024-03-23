b60f2a8bc4931fe2c22bfcdce827ddd3c6e0b3bc
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-03-23 14:33:13 +0530

    Time: 48 ms (98.79%) | Memory: 65.7 MB (25.75%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+       ios_base::sync_with_stdio(0);
        vector<int> v(nums.size(),0);
        for(int i=0;i<nums.size();i++){
             v[nums[i]-1]++;
