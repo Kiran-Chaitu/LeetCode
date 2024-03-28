899203463518e79d2b0aa0f3402cdab5519f7b09
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-03-28 22:28:12 +0530

    Time: 147 ms (99.24%) | Memory: 148.2 MB (99.67%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(0);
         unordered_map<int,int> mp;
         int l=0,r=0,maxi=INT_MIN;
         while(r<nums.size()){
