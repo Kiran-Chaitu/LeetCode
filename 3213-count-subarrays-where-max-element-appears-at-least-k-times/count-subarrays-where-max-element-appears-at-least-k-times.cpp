9117cc936ab6d521e44a1c7601ec39cdc04db2c4
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-03-29 23:34:32 +0530

    Time: 76 ms (98.45%) | Memory: 120.6 MB (48.06%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(0);
         long long ans=0,maxi=-1,l=0,r=0,n=nums.size();
         for(int i=0;i<n;i++){
             if(maxi<nums[i]) maxi = nums[i];
