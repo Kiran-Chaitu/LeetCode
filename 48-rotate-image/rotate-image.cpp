7abad47835c0b06de8a18435986b99ccae7bc24f
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-04-03 06:00:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (21.47%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        ios_base::sync_with_stdio(0);
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<=i;j++){
                 swap(matrix[i][j],matrix[j][i]);
