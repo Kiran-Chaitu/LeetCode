9faff407bd052240155d91a74fdeb5336d822678
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-04-05 09:23:08 +0530

    Time: 4 ms (42.41%) | Memory: 8.9 MB (8.61%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+        //st.push(s[0]);
+        for(int i=0;i<s.size();i++){
+            if(!st.empty() and islower(st.top()) and isupper(s[i]) and (tolower(s[i]) == st.top())){
+                cout<<st.top();
+                st.pop();
+                
+            }
+            else if(!st.empty() and isupper(st.top()) and islower(s[i]) and (toupper(s[i]) == st.top())){
+                cout<<st.top();
+                st.pop();
+            }
+            else st.push(s[i]);
+        }
+        s="";
+        while(!st.empty()){
+            s=st.top() + s;
+            st.pop();
+        }
+        return s;
+    }
+};
