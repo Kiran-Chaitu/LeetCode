c3c3c890cadca824f9eb792b8c80ad9ec8a3fbcd
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-04-13 10:21:37 +0530

    Time: 48 ms (98.06%) | Memory: 70.1 MB (93.16%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        //Code written By ___Kiran__Chaitu___
+        ios_base::sync_with_stdio(0);
         int gs=-100000,ms=-100000;
         for(int i=0;i<nums.size();i++){
             int sum=ms+nums[i];
