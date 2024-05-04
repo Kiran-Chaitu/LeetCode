eacdb21928173a9fa3c7c3615eb9ce1a305a73ef
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-05-04 19:27:03 +0530

    Time: 28 ms (99.85%) | Memory: 45.5 MB (8.97%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
         sort(people.begin(),people.end());
         int ans=0,n=people.size(),f=0,l=n-1;
         while(f<=l){
             if(people[f] + people[l] <= limit){
                 f++;
                 l--;
-                ans++;
             }
-            else{
-                ans++;
-                l--;
-            }
+            else l--;
+            ans++;
         }
         return ans;
     }
