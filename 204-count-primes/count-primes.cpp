cf613e48ee22e52ca9d21b2806f9ec81216eb573
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-05-13 21:32:09 +0530

    Time: 581 ms (5.09%) | Memory: 11.5 MB (67.09%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int seive(int n){
+        vector<bool> v(n+1,true);
+        v[0]=false,v[1]=false;
+        for(int i=2;i<=sqrt(n);i++){
+            if(v[i]){
+                for(int j=i*i;j<=n;j+=i) if(v[j]) v[j]=false;
+            }
+        }
+        int c=0;
+        for(int i=2;i<n;i++) if(v[i]) c++;
+        return c;
+    }
+    int countPrimes(int n) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
+        return seive(n);
+    }
+};
