66d7540a0d8f09a987b6c9b8680cd57f4d6e1991
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-06-07 05:56:33 +0530

    Time: 102 ms (6.98%) | Memory: 32.3 MB (18.66%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if(hand.size()%groupSize!=0) return false;
+        map<int,int> mp;
+        for(int i=0;i<hand.size();i++){
+            mp[hand[i]]++;
+        }
+        while(mp.size()!=0){
+            //cout<<mp.size()<<"\n";
+            vector<int> v;
+            int j=0;
+            for(auto i: mp){
+                if(j==groupSize) break;
+                v.push_back(i.first);
+                cout<<i.first<<" ";
+                mp[i.first]--;
+                if(j!=0){
+                    if(abs(i.first - v[j-1]) != 1) return false;
+                }
+                j++;
+                if(mp[i.first]==0) mp.erase(i.first);
+                
+            }
+            if(v.size()!=groupSize) return false;
+            cout<<"\n";
+        }
+        return true;
+        
+    }
+};
