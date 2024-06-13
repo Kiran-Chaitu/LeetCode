a16ee3839b3db6b9a22930a8a8d55074a7a24e62
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-06-13 09:34:29 +0530

    Time: 3 ms (91.43%) | Memory: 21.2 MB (17.35%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
         sort(seats.begin(),seats.end());
         sort(students.begin(),students.end());
         int c=0;
