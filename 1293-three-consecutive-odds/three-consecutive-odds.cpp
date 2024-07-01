cbb85413bc069f2dcf59bb3e9440e4b009d8098a
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-07-01 14:34:27 +0530

    Time: 6 ms (25.20%) | Memory: 10.4 MB (99.07%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkIfOdd(int n){
+        if(n&1) return true;
+        return false;
+    }
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        if(n<3) return false;
+        int oddCount = 0;
+        for(int i=0;i<3;i++){
+            if(checkIfOdd(arr[i])) oddCount++;
+        }
+        if(oddCount==3) return true;
+        for(int i=3;i<n;i++){
+            if(checkIfOdd(arr[i])) oddCount++;
+            if(checkIfOdd(arr[i-3])) oddCount--;
+            if(oddCount==3) return true;
+        }
+        return false;
+
+    }
+};
