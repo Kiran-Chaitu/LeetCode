4e43bf18eec64921bfc2adbf4215c4e7d3dd3422
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-07-10 11:18:48 +0530

    Time: 6 ms (40.12%) | Memory: 14 MB (61.11%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
         int c=0;
         for(int i=0;i<logs.size();i++){
             int n = logs[i].size();
