d78a7e8bfe3a7c200bf510be07ba44fe664a1b2a
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-07-30 20:01:28 +0530

    Time: 8 ms (76.94%) | Memory: 17.4 MB (5.42%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         ios_base::sync_with_stdio(0);
-        
         unordered_map<int,int> row,col;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
