a916d8df797d42bcdb3618a4dd90ce68515b8775
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-08-01 06:53:00 +0530

    Time: 11 ms (14.30%) | Memory: 17.7 MB (16.71%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
         int ans = 0;
-        
         for(int i=0;i<details.size();i++){
             string str = "";
             str+=(details[i][11]);
