3b4bd4ed7783355a68082dc1689918a90ef97d88
KIRAN CHAITU <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-08-31 20:43:17 +0530

    Time: 12 ms (55.74%) | Memory: 19.2 MB (70.82%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
         int n=nums.size();
         if(n==1) return 0;
         if(nums[0] >= n-1) return 1;
