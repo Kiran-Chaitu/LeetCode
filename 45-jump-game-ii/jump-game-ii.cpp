518b59b796ca00631cf7535b81a360ba73a36c99
KIRAN CHAITU <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-08-31 20:42:30 +0530

    Time: 11 ms (63.47%) | Memory: 19.3 MB (38.67%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return 0;
+        if(nums[0] >= n-1) return 1;
+        int i=0,c=0;
+        while(i<n){
+            int k=nums[i],maxi=INT_MIN,temp;
+            if(i>=n-1) return c;
+            for(int j=i+1;j<=i+k;j++){
+                if(nums[j]+j > maxi){
+                    maxi = nums[j]+j;
+                    temp=j;
+                }
+            }
+            i=temp;
+            c++;
+            if(nums[i] + i>=n-1) return c+1;
+        }
+        return -1;
+    }
+};
