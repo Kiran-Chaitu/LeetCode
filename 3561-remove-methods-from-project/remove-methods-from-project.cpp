639583b955f6e8fa66f8d873681344a1ffca9377
KIRAN CHAITU <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-10-06 11:11:30 +0530

    Time: 1064 ms (8.33%) | Memory: 348.3 MB (8.33%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        unordered_map<int,vector<int>> gp;
+        for(auto i:invocations){
+            gp[i[0]].push_back(i[1]);
+        }
+        vector<bool> sp(n,false);
+        stack<int> s;
+        s.push(k);
+        while(!s.empty()){
+            int a = s.top();
+            s.pop();
+            if(!sp[a]){
+                sp[a]=true;
+                for(int i:gp[a]){
+                    if(!sp[i]){
+                        s.push(i);
+                    }
+                }
+            }
+        }
+        for(int i=0;i<sp.size();i++) cout<<sp[i]<<" ";
+        cout<<endl;
+        // vector<bool> tor(n,true);
+        // for(int i=0;i<n;i++){
+        //     if(!sp[i]){
+        //         for(int j:gp[i]){
+        //             if(sp[j]){
+        //                 tor[i]=false;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+        vector<int> fn;
+        bool check = false;
+        for(int i=0;i<n;i++){
+            if(!sp[i]){
+                for(int j:gp[i]){
+                    if(sp[j]){
+                        check = true;
+                        break;
+                    }
+                }
+            }
+            if(check) break;
+        }
+        cout<<check<<endl;
+        if(check){
+            for(int i=0;i<n;i++){
+                fn.push_back(i);
+            }
+        }
+        else{
+            for(int i=0;i<n;i++){
+                if(!sp[i]){
+                    fn.push_back(i);
+                }
+            }
+        }
+        return fn;
+    }
+};
