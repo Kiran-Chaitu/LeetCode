14f46774d8b54f32c4950d4d4a8e0a1993679e91
KIRAN CHAITU <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-01-02 13:21:20 +0530

    Time: 223 ms (5.64%) | Memory: 26.1 MB (54.28%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,48 @@
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<pii>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define F first
+#define S second
+
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+const ll lx = LLONG_MAX;
+const ll ln = LLONG_MIN;
+
+class Solution {
+public:
+    bool solver( vi &v,ll k,ld mid){
+        ll sum=0;
+        for(int i=0;i<sz(v);i++){
+            ld temp = v[i];
+            sum+=ceil(temp/mid);
+            //cout<<sum<<" ";
+        }
+        //cout<<endl;
+        return sum<=k;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        ll maxi=nums[0] , mini = nums[0], n = sz(nums);
+        for(auto i:nums){
+            if(maxi < i) maxi =i;
+        }
+        ll l=1,r=maxi,ans;
+        while(l<=r){
+            ll mid = (l+r)>>1;
+            //cout<<mid<<endl;
+            if(solver(nums,threshold,mid)){
+                ans = mid;
+                r = mid -1;
+            }
+            else l = mid +1;
+        }
+        return ans;
+    }
+};
