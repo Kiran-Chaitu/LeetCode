8eb14a83446ddbb30efe41d270fb0d7d21e1843b
KIRAN CHAITU <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-01-03 06:04:38 +0530

    Time: 4 ms (47.38%) | Memory: 89.4 MB (44.20%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int waysToSplitArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
         long long int c=0,sum=0,temp=0,n=nums.size();
         for(auto i:nums) sum+=i;
         for(int i=0;i<n-1;i++){
