034cc2bd0abbc426c973c6a85f173d352e400a5c
KIRAN CHAITU <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-01-10 06:23:48 +0530

    Time: 388 ms (5.02%) | Memory: 181.5 MB (5.02%) - LeetSync

---
+++ 952-word-subsets/word-subsets.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> ans;
+        unordered_map<char,int> mp;
+        for(auto i: words2){
+            unordered_map<char,int> temp;
+            for(auto j : i){ 
+                temp[j]++;
+                if(mp[j]< temp[j]){
+                    mp[j] = temp[j];
+                    // cout<<mp[j]<<" "<<j<<endl;
+                }
+            }
+        }
+        for(auto i:words1){
+            unordered_map<char,int> temp;
+            for(auto j:i) temp[j]++;
+            int c=0;
+            for(auto k : mp){
+                if(mp[k.first] <= temp[k.first]) c++;
+                // cout<<mp[k.first] <<" "<<temp[k.first]<<" k.first ="<<k.first<<endl;
+            }
+            //cout<<"size = "<<mp.size()<<endl;
+            if(c == mp.size())  ans.push_back(i);
+            // cout<<endl;
+        }
+        return ans;
+    }
+};
