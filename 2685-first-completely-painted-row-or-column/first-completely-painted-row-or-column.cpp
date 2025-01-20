fe08871a51e96a52030cfa84c103abc08f6a7457
KIRAN CHAITU <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-01-20 17:55:20 +0530

    Time: 226 ms (13.72%) | Memory: 211.8 MB (6.10%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
         int n  = mat.size() , m = mat[0].size();
         unordered_map<int,int> mp;
         unordered_map<int,int> rows,cols;
@@ -10,14 +12,11 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 row_col[mat[i][j]] = {i,j};
-                // cout<<"{ "<<row_col[mat[i][j]].first<<" "<<row_col[mat[i][j]].second<<" } ";
             }
-            // cout<<endl;
         }
         for(int i=0;i<arr.size();i++){
             rows[row_col[arr[i]].first]++;
             cols[row_col[arr[i]].second]++;
-            // cout<<"row sum = "<<rows[row_col[arr[i]].first]<<" col sum = "<<cols[row_col[arr[i]].second]<<endl;
             if(rows[row_col[arr[i]].first] == m || cols[row_col[arr[i]].second] == n) return i;
         }
         return -1;
