9027b72faec844bf35c7d8f192713448bf4a5a53
KIRAN CHAITU <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-02-24 21:11:13 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (59.31%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,12 +4,8 @@ public:
         vector<int> tmp;
         int n=nums.size();
         if(k>=n) k=k%n; 
-        for(int i=n-k;i<n;i++){
-            tmp.push_back(nums[i]);
-        }
-        for(int i=n-k-1;i>=0;i--){
-            nums[i+k] = nums[i];
-        }
-        for(int i=0;i<k;i++) nums[i] = tmp[i];
+        reverse(nums.begin() , nums.end() -k);
+        reverse(nums.end() - k, nums.end());
+        reverse(nums.begin() , nums.end());
     }
 };
