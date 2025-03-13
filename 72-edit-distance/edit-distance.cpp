7d5da75c0314bb9da18fb114b19f227515b4c0ee
KIRAN CHAITU <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-03-13 11:14:52 +0530

    Time: 5 ms (71.89%) | Memory: 13.2 MB (30.97%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int solver(int i , int j , string &s1, string &s2, vector<vector<int>> &dp){
-        if(i<0){
-            return j+1;
+        if(i==0){
+            return j;
         } 
-        if(j<0) return i+1;
+        if(j==0) return i;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i] == s2[j]) return solver(i-1,j-1,s1,s2,dp);
+        if(s1[i-1] == s2[j-1]) return solver(i-1,j-1,s1,s2,dp);
         dp[i][j] =  1 + min(solver(i,j-1,s1,s2,dp) , min(solver(i-1,j,s1,s2,dp),
         solver(i-1,j-1,s1,s2,dp)));
         return dp[i][j];
     }
     int minDistance(string word1, string word2) {
         int n = word1.size() , m = word2.size();
-        vector<vector<int>> dp(n , vector<int>(m,-1));
-        return solver(n-1,m-1 ,word1,word2,dp);
+        vector<vector<int>> dp(n +1, vector<int>(m+1,-1));
+        return solver(n,m ,word1,word2,dp);
     }
 };
