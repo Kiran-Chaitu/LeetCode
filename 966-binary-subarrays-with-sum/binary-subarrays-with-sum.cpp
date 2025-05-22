d8f8bca7666fab4b0cffe6e0c874a5b5a43123b6
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-05-22 09:53:08 +0530

    Time: 3 ms (56.82%) | Memory: 32.3 MB (99.98%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        ios_base::sync_with_stdio(0);
+    int solver(vector<int> &nums , int k){
         int c=0;
-        if(goal==0){
-            int l=0,i=0;
-            while(i<nums.size()){
-                if(nums[i]==0) l++;
-                else{
-                    c+=(l*(l+1))/2;
-                    l=0;
-                }
-                i++;
+        if(k<0) return 0;
+        int l= 0 , r = 0, n = nums.size() , sum =0;
+        while(r < n){
+            sum+=nums[r];
+            while(sum > k){
+                sum-=nums[l++];
             }
-            if(nums[nums.size()-1]==0)  c+=(l*(l+1))/2;
-            return c;
-        }
-        int sum=0,ps=0;
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
-            ps+=nums[i];
-            if(nums[i]==0) mp[ps]++;
-            if(ps==goal){
-                c+=(1+mp[0]);
-            } 
-            else if(ps > goal){
-                c+=(mp[ps - goal] + 1);
-            }
+            c += (r-l+1);
+            r++;
         }
         return c;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        ios_base::sync_with_stdio(0);
+        return solver(nums,goal) - solver(nums , goal-1);
     }
 };
