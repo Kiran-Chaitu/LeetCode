b1c56352a65580ebcb1260bdccaef199c2931c8b
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-05-22 09:57:21 +0530

    Time: 6 ms (69.10%) | Memory: 71.9 MB (34.20%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
+    int solver(vector<int> &nums , int k){
+        int c=0;
+        if(k<0) return 0;
+        int l= 0 , r = 0, n = nums.size() , sum =0;
+        while(r < n){
+            sum+=nums[r];
+            while(sum > k){
+                sum-=nums[l++];
+            }
+            c += (r-l+1);
+            r++;
+        }
+        return c;
+    }
     int numberOfSubarrays(vector<int>& nums, int k) {
         ios_base::sync_with_stdio(0);
         cin.tie(NULL);
-        int oc=0,i=0,j=0,ans=0,n=nums.size();
-        unordered_map<int,int> mp;
-        mp[0]=-1;
-        for(int i=0;i<n;i++){
-            if(nums[i]%2!=0){
-                oc++;
-                mp[oc]=i;
-            }
-        }
-        oc++;
-        if(mp.size()!=1){
-            mp[oc]=n;
-            for(auto i:mp){
-                if(i.first>=k and mp[i.first]!=n){
-                    int a = (mp[i.first - k +1]) - (mp[i.first - k] +1)  +1;
-                    int b = (mp[i.first+1] - 1) - (mp[i.first]) +1;
-                    ans+=(a*b);
-                }
-            }
-        }
-        return ans;
+        for(int i=0;i<nums.size();i++) if(nums[i]&1) nums[i]=1; else nums[i]=0;
+        return solver(nums , k) - solver(nums , k -1);
     }
 };
