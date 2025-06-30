5ff3e993d9f2d36b41ef3cfc8379094b3c9cc48e
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-06-30 11:58:28 +0530

    Time: 71 ms (5.09%) | Memory: 57.6 MB (5.69%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -6,10 +6,6 @@ public:
             mp[nums[i]]++;
         }
         int maxi = 0;
-        for(auto i: mp){
-            cout<<i.first<<" ->"<<i.second<<endl;
-        }
-        cout<<endl;
         for(int i=0; i< nums.size();i++){
             if(mp[ nums[i] +1] > 0){
                 maxi = max(maxi , mp[nums[i]] + mp[nums[i]+1]);
