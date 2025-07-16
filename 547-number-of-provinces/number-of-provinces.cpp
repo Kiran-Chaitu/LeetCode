34b9f818b2d4998c0314440b3d8d9095a03fdd71
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-07-16 14:59:35 +0530

    Time: 14 ms (6.02%) | Memory: 20.5 MB (42.16%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void dfs(int i , unordered_map<int,vector<int>> &mp , vector<bool> &vis){
+        if(vis[i] || mp.find(i)==mp.end()) return;
+        cout<<i<<" ";
+        vis[i] = true;
+        for(auto j : mp[i]){
+            dfs(j , mp , vis);
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        unordered_map<int,vector<int>> mp;
+        for(int i=0;i<n;i++){
+            for(int j = 0 ;j<n;j++){
+                if(i!=j and isConnected[i][j]){
+                    mp[i].push_back(j);
+                }
+            }
+        }
+        // for(auto i : mp){
+        //     cout<<i.first<<" -> ";
+        //     for(auto j : i.second) cout<<j<<" ";
+        //     cout<<endl;
+        // }
+        if(mp.size() == 0) return isConnected.size();
+        vector<bool> vis(n,false);
+        int ans =0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                cout<<"i = "<<i<<endl;
+                if(mp[i].size() > 0) dfs( i , mp , vis );
+                else vis[i] = true;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
