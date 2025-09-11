0b5fd6be8537ffd8f6e1bdf4bdb5bc8d4ee6015e
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2025-09-11 09:46:55 +0530

    Time: 30 ms (10.44%) | Memory: 19.3 MB (13.31%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    
-    bool isVowel(char ch){
-        unordered_map<char , int> mp;    
-        vector<char> vowels = {'a' , 'e' ,'i' , 'o' , 'u' , 'A' , 'E' , 'I' , 'O' , 'U'};
-        for(auto i : vowels) mp[i]=1;
-        return mp[ch];
-
-    }
     string sortVowels(string s) {
+        unordered_map<char , int> mp;    
+        vector<char> vow = {'a' , 'e' ,'i' , 'o' , 'u' , 'A' , 'E' , 'I' , 'O' , 'U'};
+        for(auto i : vow) mp[i]=1;
         vector<char> vowels = {};
         for(auto i : s){
-            if(isVowel(i)) vowels.push_back(i);
+            if(mp[i]) vowels.push_back(i);
         } 
         sort(vowels.begin() , vowels.end());
         int i = 0;
         for(int j = 0 ; j < s.size() ;j++){
-            if(isVowel(s[j])){
+            if(mp[s[j]]){
                 s[j] = vowels[i++];
             }
         }
