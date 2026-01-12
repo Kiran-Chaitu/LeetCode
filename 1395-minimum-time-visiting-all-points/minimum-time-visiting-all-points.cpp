838482bc8e29ef7c4cc0ff87d935ccbba45893e4
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-01-12 23:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (26.76%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -5,7 +5,7 @@ public:
         int ans =0  , n =  points.size();
         for(int i =0 ; i < n-1 ; i++){
             int x = points[i][0] , y = points[i][1] , tarx = points[i+1][0] , tary = points[i+1][1];
-            int tempx = 0 , tempy =0 , a = 0 , b = 0;
+            int tempx = 0 , tempy =0 ;
             tempx = abs(tarx - x) ;
             tempy = abs(tary - y);
             ans+=max(tempx,tempy);
