be0101859c2d6b79f9b7c6ed93aa9c2491a635f1
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-03-21 18:58:12 +0530

    Time: 666 ms (11.92%) | Memory: 375.8 MB (9.56%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    void dfs(unordered_map<int,vector<int>> &mp , int node ,vector<bool> &vis, bool &ans , int dest){
+    void dfs(int node , vector<bool> &vis , unordered_map<int,vector<int>> &adj , int dest){
         if(vis[node]) return;
-        vis[node] = true;
         if(node == dest){
-            ans = true;
+            vis[node] = true;
             return;
         }
-        for(auto i : mp[node]){
-            if(!vis[i]){
-                dfs(mp , i ,vis, ans,dest);
-            }
+        vis[node] = true;
+        for(auto i : adj[node]){
+            dfs(i , vis , adj ,dest);
         }
-    } 
+
+    }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int,vector<int>> mp;
+        vector<bool> vis(n , false);
+        unordered_map<int, vector<int>> adjList;
         for(auto i : edges){
-            mp[i[0]].push_back(i[1]);
-            mp[i[1]].push_back(i[0]);
+            adjList[i[0]].push_back(i[1]);
+            adjList[i[1]].push_back(i[0]);
         }
-        bool ans = false;
-        vector<bool> vis(n,false);
-        dfs(mp , source , vis, ans , destination);
-        return ans;
+        dfs(source , vis , adjList , destination);
+        return vis[destination];
     }
 };
