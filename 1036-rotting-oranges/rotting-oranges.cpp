06a5c6c34ea1ed054c9cb953424bc58693ee2497
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-03-21 20:42:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (7.96%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& mat) {
-        queue<pair<int,int>> q;
-        int ans=0;
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[0].size();j++){
-                if(mat[i][j] == 2) q.push({i,j});
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size() , n = grid[0].size();
+        vector<vector<int>> vis( m , vector<int>(n,0));
+        queue<vector<int>> q;
+        for(int i = 0; i < m ; i++){
+            for(int j = 0; j < n ; j++){
+                if(grid[i][j] == 2){
+                    q.push({i,j,0});
+                    vis[i][j]=1;
+                }
             }
         }
+        int ans = 0;
+        vector<pair<int,int>> directions = { {-1 , 0} , {1,0} , {0,-1} , {0,1}};
         while(!q.empty()){
-            int n = q.size();
-            for(int k=0;k<n;k++){
-                int i=q.front().first , j =q.front().second;
-                if(i-1>=0 and mat[i-1][j]==1){
-                    mat[i-1][j]=2;
-                    q.push({i-1,j});
+            int i = q.front()[0] , j  = q.front()[1] , time = q.front()[2];
+            ans = max(ans , time);
+            for(auto k : directions){
+                int newRow = i+ k.first , newCol = j + k.second;
+                if(newRow >=0 and newRow < m  and newCol >=0  and newCol <n and grid[newRow][newCol]==1 and !(vis[newRow][newCol])){
+                    grid[newRow][newCol] = 2;
+                    vis[newRow][newCol]=1;
+                    q.push({newRow , newCol , time+1});
                 }
-                if(i+1<mat.size() and mat[i+1][j]==1){
-                    mat[i+1][j]=2;
-                    q.push({i+1,j});
-                }
-                if(j+1 < mat[0].size() and mat[i][j+1]==1){
-                    mat[i][j+1] =2;
-                    q.push({i,j+1});
-                }
-                if(j-1 >=0 and mat[i][j-1]==1){
-                    mat[i][j-1] = 2;
-                    q.push({i,j-1});
-                }
-                q.pop();
             }
-            ans++;
+            q.pop();
         }
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[0].size();j++){
-                if(mat[i][j] == 1) return -1;
+        for(int i = 0; i < m ; i++){
+            for(int j = 0; j < n ; j++){
+                if(grid[i][j] == 1) return -1;
             }
         }
-        if(ans==0) return 0;
-        return ans-1;
+        return ans;
     }
 };
