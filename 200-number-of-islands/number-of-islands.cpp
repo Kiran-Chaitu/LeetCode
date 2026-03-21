8ac07bc00f0a742870063a184195daafc381507f
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-03-21 20:05:57 +0530

    Time: 23 ms (83.55%) | Memory: 25 MB (9.73%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -2,25 +2,18 @@ class Solution {
 public:
     void bfs(int i , int j ,int m ,int n,vector<vector<char>> &grid , vector<vector<int>> &vis){
         vis[i][j] = 1;
+        vector<pair<int,int>> directions = { {-1,0} , { 0 , -1} , {1,0} , {0,1} };
         queue<pair<int,int>> q;
         q.push({i,j});
         while(!q.empty()){
             int r = q.front().first , c = q.front().second;
-            if(r >=0 and r <m and c+1 >=0 and c+1 < n and grid[r][c+1] == '1' and !(vis[r][c+1])){ // right
-                vis[r][c+1]=1;
-                q.push({r,c+1});
-            }
-            if(r >=0 and r <m and c-1 >=0 and c-1 < n and grid[r][c-1] == '1' and !(vis[r][c-1])){ // left
-                vis[r][c-1]=1;
-                q.push({r,c-1});
-            }
-            if(r-1 >=0 and r-1 <m and c >=0 and c < n and grid[r-1][c] == '1' and !(vis[r-1][c])){ // up
-                vis[r-1][c]=1;
-                q.push({r-1,c});
-            }
-            if(r+1 >=0 and r+1 <m and c >=0 and c < n and grid[r+1][c] == '1' and !(vis[r+1][c])){ // down
-                vis[r+1][c]=1;
-                q.push({r+1,c});
+            for(auto i : directions){
+                int newRow = r + i.first;
+                int newCol = c + i.second;
+                if(newRow >=0 and newRow < m and newCol >=0 and newCol < n and grid[newRow][newCol] == '1' and !(vis[newRow][newCol])){
+                    vis[newRow][newCol] = 1;
+                    q.push({newRow , newCol});
+                }
             }
             q.pop();
         }
