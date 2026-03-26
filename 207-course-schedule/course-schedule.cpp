a3f28746c19d1a7fb973fc93bc666a1b53617b92
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-03-26 16:25:40 +0530

    Time: 7 ms (47.13%) | Memory: 21 MB (10.35%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,55 +1,32 @@
 class Solution {
 public:
-    // bool dfs(unordered_map<int,vector<int>> &mp, vector<int> &vis , vector<int> &pathVis , int val){
-    //     if(vis[val]){
-    //         if(pathVis[val]) return true;
-    //         return false;
-    //     }
-    //     vis[val] = 1;
-    //     pathVis[val] = 1;
-    //     bool temp = false;
-    //     for(auto i : mp[val]){
-    //         temp |= dfs(mp , vis,pathVis , i);
-    //     }
-    //     pathVis[val] =0;
-    //     return temp;
-    // }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // DFS Approach
-        // unordered_map<int,vector<int>> mp;
-        // vector<int> vis(numCourses) , pathVis(numCourses);
-        // for(int i=0;i<prerequisites.size();i++){
-        //     mp[prerequisites[i][0]].push_back(prerequisites[i][1]);
-        // }
-        // for(int i =0 ; i  < numCourses ; i++){
-        //     if(!vis[i]){
-        //         if(dfs(mp , vis , pathVis , i)) return false;
-        //     }
-        // }
-        // return true;
-
-        //Topo Sort
-        vector<int> inorder(numCourses) , ans;
-        unordered_map<int, vector<int>> mp; 
-        queue<int> q;
-        for(auto i : prerequisites){
-            inorder[i[0]]++;
-            mp[i[1]].push_back(i[0]);
-        }
-        for(int i =0 ; i < numCourses;i++) if(inorder[i] == 0) q.push(i);
-        // for(int i =0 ; i  <numCourses;i++) cout<<inorder[i]<<" ";
-        // cout<<endl;
-        while(!q.empty()){
-            int nod = q.front();
-            q.pop();
-            for(auto i : mp[nod]){
-                inorder[i]--;
-                if(inorder[i] == 0) q.push(i);
+    bool dfs(int node ,vector<bool> &vis , vector<bool> &path,  unordered_map<int,vector<int>> &adj){
+        vis[node]=true;
+        path[node] = true;
+        for(auto i : adj[node]){
+            if(!vis[i]){
+                if(dfs(i , vis, path, adj)) return true;
             }
-            ans.push_back(nod);
+            else{
+                if(path[i]) return true;
+            }
         }
-        // for(int i = 0 ; i < ans.size();i++) cout<<ans[i]<<" ";
-
-        return ans.size() == numCourses;
+        path[node] = false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>> mp;
+        for(auto i : prerequisites){
+            mp[i[0]].push_back(i[1]);
+        }
+        vector<bool> vis(numCourses+1 , false) , path(numCourses+1 , false);
+        for(int i =1 ;i <=numCourses ;i++){
+            if(!vis[i]){
+                if(dfs(i , vis , path , mp)){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 };
