ba086c3bf75193bad9a6183893444a4ba452f887
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-03-28 19:48:17 +0530

    Time: 59 ms (57.22%) | Memory: 22.1 MB (25.54%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string,int> mp;
+        for(auto i : wordList){
+            mp[i]=1;
+        }
         queue<pair<string,int>> q;
-        unordered_map<string , int> mp;
-        for(auto i : wordList) mp[i]++;
-        if(mp.find(endWord)== mp.end()) return 0;
         q.push({beginWord , 1});
-        if(mp.find(beginWord)!=mp.end()) mp.erase(beginWord);
+        if(mp.find(beginWord)!=mp.end()){
+            mp.erase(beginWord);
+        }
         while(!q.empty()){
-            if(q.front().first == endWord) return q.front().second;
             string word = q.front().first;
             int level = q.front().second;
-            for(int i = 0; i < word.size() ;i++ ){
-                char original = word[i];
-                for(char ch = 'a' ; ch <='z';ch++){
-                    word[i] = ch;
-                    if(mp.find(word)!=mp.end()){ 
+            q.pop();
+            if(word == endWord) return level;
+            for(int i = 0; i < word.size();i++){
+                char org = word[i];
+                for(auto alp = 'a' ; alp <='z';alp++){
+                    word[i] = alp;
+                    if(mp.find(word)!=mp.end()){
                         q.push({word , level+1});
                         mp.erase(word);
                     }
                 }
-                word[i] = original;
+                word[i] = org;
             }
-            // cout<<endl;
-            q.pop();
         }
         return 0;
     }
