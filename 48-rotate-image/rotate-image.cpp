20fd67afbb7aa818217120bfb448afe9f14f7a9e
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-05-04 09:41:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (68.52%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(NULL);
         int n = matrix.size();
         for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                int t = matrix[i][j];
+            for(int j = 0;j <=i;j++){
+                int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
-                matrix[j][i] = t;
+                matrix[j][i] = temp;
             }
         }
         for(int i=0;i<n;i++){
-            for(int j=0;j<n/2;j++){
-                int t = matrix[i][j];
+            for(int j = 0; j < n/2;j++){
+                int temp = matrix[i][j];
                 matrix[i][j] = matrix[i][n-j-1];
-                matrix[i][n-1-j] = t;
+                matrix[i][n-1-j] = temp;
             }
         }
     }
