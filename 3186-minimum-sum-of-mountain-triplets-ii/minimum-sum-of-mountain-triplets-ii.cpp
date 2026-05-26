ae8b5263f8a47ca04b7fb1db9b1a069c52561a3b
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-05-26 13:53:43 +0530

    Time: 11 ms (15.83%) | Memory: 85.8 MB (52.08%) - LeetSync

--- 3186-minimum-sum-of-mountain-triplets-ii/minimum-sum-of-mountain-triplets-ii.cpp
+++ 3186-minimum-sum-of-mountain-triplets-ii/minimum-sum-of-mountain-triplets-ii.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int minimumSum(vector<int>& nums) {
-        map<int,int> pre,suf;
+        int n = nums.size();
+        vector<int> pre(n),suf(n);
         int ans = INT_MAX;
-        for(auto i  : nums) suf[i]++;
+        for(int i =0 ; i < n; i++){
+            pre[i] = ans;
+            ans = min(ans, nums[i]);
+        }
+        ans = INT_MAX;
+        for(int i=n-1;i>=0;i--){
+            suf[i] = ans;
+            ans = min(ans, nums[i]);
+        }
+        ans = INT_MAX;
         for(int i = 0; i < nums.size();i++){
-            suf[nums[i]]--;
-            if(suf[nums[i]] <= 0) suf.erase(nums[i]);
-            if(!(pre.empty()) and !(suf.empty())){
-                int left = pre.begin()->first , right = suf.begin()->first;
-                if(left < nums[i] and right < nums[i])    ans = min(ans, left + nums[i] + right);
+            if(pre[i] < nums[i] and suf[i] < nums[i]){
+                ans = min(ans, pre[i] + nums[i] + suf[i]);
             }
-            pre[nums[i]]++;
         }
         return (ans == INT_MAX) ? -1 : ans;
     }
