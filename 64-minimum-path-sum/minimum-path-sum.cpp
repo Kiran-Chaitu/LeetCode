790bb53d970476f3891614d7cd125699d2611732
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-06-13 15:58:46 +0530

    Time: 182 ms (5.17%) | Memory: 16.9 MB (15.28%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int solver(int i, int j ,vector<vector<int>> &dp , vector<vector<int>> &grid){
-        if(i<0 || j <0) return INT_MAX;
-        if(i==0 and j ==0 ) return grid[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up  = solver(i-1,j,dp,grid);
-        int left = solver(i,j-1,dp,grid);
-        return dp[i][j] = grid[i][j] + min(up, left);
-    }
+    // int solver(int i, int j ,vector<vector<int>> &dp , vector<vector<int>> &grid){
+    //     if(i<0 || j <0) return INT_MAX;
+    //     if(i==0 and j ==0 ) return grid[i][j];
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int up  = solver(i-1,j,dp,grid);
+    //     int left = solver(i,j-1,dp,grid);
+    //     return dp[i][j] = grid[i][j] + min(up, left);
+    // }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size() , n = grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n,-1));
