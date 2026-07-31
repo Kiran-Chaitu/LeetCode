2419190d6207aa1eb2e882ea0e3bf0d15d58a88c
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-07-31 10:38:58 +0530

    Time: 9 ms (53.64%) | Memory: 25.5 MB (61.34%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(NULL);
-        unordered_map<char,int> mp;
-        for(int i=0;i<word.size();i++){
-            mp[word[i]]++;
+        vector<int> mp(26);
+        for(auto i : word){
+            mp[i - 'a']++;
         }
-        vector<pair<char,int>> vec(mp.begin(),mp.end());
-        sort(vec.begin(),vec.end(),[](pair<char,int> &a,pair<char,int> &b){
-            return a.second > b.second;
+        sort(mp.begin() , mp.end() , [](int a , int b){
+            return a > b;
         });
-        int ans=0;
-        for(int i=0;i<vec.size();i++){
-            float temp = i;
-            ans+=(vec[i].second*ceil((temp+1)/8));
-            //cout<<ans<<" "<<vec[i].second<<" "<<ceil(temp+1/8)<<endl;
-
+        int ans = 0;
+        for(int i = 0; i < 26;i++){
+            if(i < 8) ans += mp[i];
+            else if(i < 16) ans+= mp[i] * 2;
+            else if(i < 24) ans += mp[i] * 3;
+            else ans += mp[i] * 4;
         }
         return ans;
     }
