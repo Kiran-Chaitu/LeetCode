5cf51562fc7ba70651cc2a34448a9b0af7da5d97
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-08-05 18:46:49 +0530

    Time: 693 ms (8.97%) | Memory: 407.8 MB (5.17%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -1,53 +1,95 @@
+// class DSU{
+//     private:
+//         vector<int> parent, size;
+//     public:
+//         DSU(int n){
+//             parent.resize(n+1,0);
+//             size.resize(n+1,1);
+//             for(int i = 0;i<=n;i++) parent[i] = i;
+//         }
+//         int findParent(int node){
+//             if(parent[node] == node) return node;
+//             return parent[node] = findParent(parent[node]);
+//         }
+//         void unionBySize(int u, int v){
+//             int pu = findParent(u) , pv = findParent(v);
+//             if(pu == pv) return;
+//             if(size[pu] <= size[pv]){
+//                 parent[pu] = pv;
+//                 size[pv] += size[pu];
+//             }else{
+//                 parent[pv] = pu;
+//                 size[pu] += size[pv];
+//             }
+//         }
+// };
+
 class Solution {
 public:
+
+    
+    void dfs(int node,unordered_map<int,vector<int>> &adj , vector<int> &vis , unordered_map<int,int> &sus){
+        vis[node] = true;
+        sus[node] = 1;
+        for(auto i : adj[node]){
+            if(!vis[i]) dfs(i, adj, vis,sus);
+        }
+    }
+
+    void solver(int node,unordered_map<int,vector<int>> &adj , vector<int> &vis , vector<bool> &temp ,unordered_map<int,int> &sus ){
+        vis[node] = true;
+        for(auto i : adj[node]){
+            if(!vis[i]) solver(i, adj, vis, temp , sus);
+            else{
+                if(sus.find(i)!=sus.end()){
+                    temp[0] = true;
+                    return;
+                }
+            }
+        }
+    }
+   
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
-        unordered_map<int,vector<int>> gp;
+        unordered_map<int,vector<int>> adj;
         for(auto i:invocations){
-            gp[i[0]].push_back(i[1]);
+            adj[i[0]].push_back(i[1]);
         }
-        vector<bool> sp(n,false);
-        queue<int> q;
-        q.push(k);
-        while(!q.empty()){
-            int a = q.front();
-            q.pop();
-            if(!sp[a]){
-                sp[a]=true;
-                for(int i:gp[a]){
-                    if(!sp[i]){
-                        q.push(i);
-                    }
-                }
+        // DSU dsu(n);
+        // for(auto i : adj){
+        //     for(auto j : i.second) dsu.unionBySize(i.first , j);
+        // }
+        vector<int> vis(n ,0) ;
+        unordered_map<int,int> sus;
+        dfs(k , adj,vis, sus);
+        vector<int> ans;
+        // for(auto i : sus) cout<<i<<" ";
+        // // cout<<endl;
+        // for(int i = 0; i < n;i++){
+        //     if(vis[i]) continue;
+        //     for(auto j : sus){
+        //         if(dsu.findParent(i) == dsu.findParent(j)){
+        //             // cout<<i<<" "<<j<<endl;
+        //             temp = false;
+        //             break;
+        //         }
+        //     }
+        // }
+
+
+        vector<bool> temp = {false};
+        for(int i = 0 ;i < n;i++){
+            if(vis[i]) continue;
+            solver(i, adj, vis, temp , sus);
+            if(temp[0]) break;
+        }
+
+        if(temp[0]){
+            for(int i = 0;i < n; i++) ans.push_back(i);
+        }else{
+            for(int i = 0; i < n ; i++){
+                if(sus.find(i) == sus.end()) ans.push_back(i);
             }
         }
-        // for(int i=0;i<sp.size();i++) cout<<sp[i]<<" ";
-        // cout<<endl;
-        vector<int> fn;
-        bool check = false;
-        for(int i=0;i<n;i++){
-            if(!sp[i]){
-                for(int j:gp[i]){
-                    if(sp[j]){
-                        check = true;
-                        break;
-                    }
-                }
-            }
-            if(check) break;
-        }
-        //cout<<check<<endl;
-        if(check){
-            for(int i=0;i<n;i++){
-                fn.push_back(i);
-            }
-        }
-        else{
-            for(int i=0;i<n;i++){
-                if(!sp[i]){
-                    fn.push_back(i);
-                }
-            }
-        }
-        return fn;
+        return ans;
     }
 };
