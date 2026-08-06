3f96038ff6c3f7a93192493b307c3fe2ac25a0e8
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-08-06 13:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (37.77%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int product(int n){
+        int pro =1;
+        while(n > 0){
+            pro *= (n%10);
+            n /= 10;
+        }
+        return pro;
+    }
+    int smallestNumber(int n, int t) {
+        
+        while( product(n) % t != 0) {
+            n++;
+        }
+        return n;
+    }
+};
