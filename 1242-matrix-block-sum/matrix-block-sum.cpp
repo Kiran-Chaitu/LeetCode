1e670b3a9a51d303a421b32a5b011213d6fe72a7
Kiran Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2026-08-14 15:07:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (23.61%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.cpp
+++ 1242-matrix-block-sum/matrix-block-sum.cpp
@@ -7,11 +7,8 @@ public:
         int n = mat.size() , m = mat[0].size();
         vector<vector<int>> ans(n , vector<int>(m ,0));
         vector<vector<int>> pre(n+1 , vector<int>(m+1 ,0));
-        pre[1][1] = mat[0][0];
-        for(int i=2;i<=n;i++) pre[i][1]+=pre[i-1][1] + mat[i-1][0];
-        for(int i=2;i<=m;i++) pre[1][i]+=pre[1][i-1] + mat[0][i-1];
-        for(int i=2;i<=n;i++){
-            for(int j=2;j<=m;j++){
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=m;j++){
                 pre[i][j] = mat[i-1][j-1] + pre[i-1][j] + pre[i][j-1] - pre[i-1][j-1];
             }
         }
